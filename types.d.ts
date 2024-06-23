fa6069213eb7c99243d8922343ba4355bac3daef
edwinochieng <ochiengedwin440@gmail.com>
2024-06-24 00:47:46 +0300

    add next-auth types

---
+++ types.d.ts
@@ -0,0 +1,28 @@
+declare module "next-auth" {
+  interface User {
+    id: string;
+    name: string;
+    email: string;
+  }
+
+  interface Account {}
+
+  interface Session {
+    user: {
+      id: string;
+      name: string;
+      email: string;
+    };
+  }
+}
+
+// The `JWT` interface can be found in the `next-auth/jwt` submodule
+import { JWT } from "next-auth/jwt";
+
+declare module "next-auth/jwt" {
+  /** Returned by the `jwt` callback and `auth`, when using JWT sessions */
+  interface JWT {
+    /** OpenID ID Token */
+    idToken?: string;
+  }
+}
