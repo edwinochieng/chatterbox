b9860e03d1a0c7b59e38144d1ed0abcde01d5b94
edwinochieng <ochiengedwin440@gmail.com>
2024-07-17 18:35:34 +0300

    add chat component

---
+++ src/components/Chat.tsx
@@ -0,0 +1,29 @@
+"use client";
+import React from "react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
+export default function Chat() {
+  return (
+    <div className="w-full flex flex-row items-center my-3 px-6 py-2 cursor-pointer hover:bg-indigo-50 ">
+      <Avatar className="h-[42px] w-[42px]">
+        <AvatarImage src="/profile.jpg" />
+        <AvatarFallback>CN</AvatarFallback>
+      </Avatar>
+
+      <div className="ml-3">
+        <div className="flex flex-row items-center justify-between">
+          <span className="text-gray-800 font-semibold text-base">
+            John Doe
+          </span>
+          <span className="text-gray-500 font-medium text-sm">4.45 PM</span>
+        </div>
+
+        <div className="flex flex-row items-center justify-between ">
+          <p className="text-sm text-gray-700 ">
+            This is a dummy text. This is a dummy text, dummy text,dummy text
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/components/ChatList.tsx
+++ src/components/ChatList.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React, { useState } from "react";
 import { CiSearch } from "react-icons/ci";
+import Chat from "./Chat";
 
 export default function ChatList() {
   const [searchTerm, setSearchTerm] = useState("");
@@ -15,33 +16,41 @@ export default function ChatList() {
     "Chat 8",
     "Chat 9",
     "Chat 10",
+    "Chat 11",
+    "Chat 12",
+    "Chat 13",
+    "Chat 14",
+    "Chat 15",
+    "Chat 16",
+    "Chat 17",
+    "Chat 18",
   ];
 
   const filteredChats = chats.filter((chat) =>
     chat.toLowerCase().includes(searchTerm.toLowerCase())
   );
   return (
-    <div className="flex flex-col p-6 h-full w-full lg:w-[300px]">
-      <h1 className="text-3xl font-semibold text-gray-800">Messages</h1>
+    <div className="relative top-0 bottom-0 left-0 h-screen w-full lg:max-w-[280px] xl:max-w-[340px]">
+      <div className="mt-5 px-6">
+        <h1 className="text-3xl font-semibold text-gray-800">Messages</h1>
 
-      <div className="flex flex-row items-center justify-between p-1 px-4 w-full my-6 border rounded-[8px] bg-gray-100">
-        <input
-          type="text"
-          placeholder="Search chats"
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-          className="flex-1 mr-2 py-2 border-none outline-none bg-transparent"
-        />
-        <div>
-          <CiSearch size={20} />
+        <div className="flex flex-row items-center justify-between p-1 px-4 w-full my-6 border rounded-[8px] bg-gray-100">
+          <input
+            type="text"
+            placeholder="Search chats"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            className="flex-1 mr-2 py-2 border-none outline-none bg-transparent"
+          />
+          <div>
+            <CiSearch size={20} />
+          </div>
         </div>
       </div>
 
-      <div className="flex-grow overflow-y-auto">
+      <div className="overflow-y-auto">
         {filteredChats.map((chat, index) => (
-          <div key={index} className="p-2 border-b">
-            {chat}
-          </div>
+          <Chat key={index} />
         ))}
       </div>
     </div>
