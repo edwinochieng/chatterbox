fa2df22f864e2bbe2d70f3fe0f8d744403bc4a4c
edwinochieng <ochiengedwin440@gmail.com>
2024-07-19 23:53:16 +0300

    style scrollbar

--- src/components/ChatList.tsx
+++ src/components/ChatList.tsx
@@ -13,45 +13,36 @@ export default function ChatList() {
     "Chat 5",
     "Chat 6",
     "Chat 7",
-    "Chat 8",
-    "Chat 9",
-    "Chat 10",
-    "Chat 11",
-    "Chat 12",
-    "Chat 13",
-    "Chat 14",
-    "Chat 15",
-    "Chat 16",
-    "Chat 17",
-    "Chat 18",
   ];
 
   const filteredChats = chats.filter((chat) =>
     chat.toLowerCase().includes(searchTerm.toLowerCase())
   );
   return (
-    <div className="absolute top-0 bottom-0 left-0 h-screen w-full lg:max-w-[280px] xl:max-w-[340px] bg-primary">
-      <div className="mt-5 px-6">
-        <h1 className="text-3xl font-semibold text-gray-800">Messages</h1>
+    <div className="absolute top-0 bottom-0 left-0 max-h-screen w-full lg:max-w-[280px] xl:max-w-[340px] bg-primary">
+      <div className="h-full overflow-y-auto custom-scrollbar">
+        <div className="mt-5 px-6">
+          <h1 className="text-3xl font-semibold text-gray-800">Messages</h1>
 
-        <div className="flex flex-row items-center justify-between p-1 px-4 w-full my-6 border rounded-[8px] bg-gray-100">
-          <input
-            type="text"
-            placeholder="Search chats"
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-            className="flex-1 mr-2 py-2 border-none outline-none bg-transparent"
-          />
-          <div>
-            <CiSearch size={20} />
+          <div className="flex flex-row items-center justify-between p-1 px-4 w-full my-6 border rounded-[8px] bg-gray-100">
+            <input
+              type="text"
+              placeholder="Search chats"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+              className="flex-1 mr-2 py-2 border-none outline-none bg-transparent"
+            />
+            <div>
+              <CiSearch size={20} />
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className="overflow-y-auto">
-        {filteredChats.map((chat, index) => (
-          <Chat key={index} />
-        ))}
+        <div>
+          {filteredChats.map((chat, index) => (
+            <Chat key={index} />
+          ))}
+        </div>
       </div>
     </div>
   );
