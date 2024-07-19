542eb2a5fa09175956dfb32e780c524b4f85a25b
edwinochieng <ochiengedwin440@gmail.com>
2024-07-20 00:58:00 +0300

    add log out button

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -4,6 +4,9 @@ import React, { useState } from "react";
 import { FaUserAlt, FaCog } from "react-icons/fa";
 import { RiMessage3Fill } from "react-icons/ri";
 import { FiMenu } from "react-icons/fi";
+import { MdLogout } from "react-icons/md";
+import { signOut } from "next-auth/react";
+import { redirect } from "next/dist/server/api-utils";
 
 const Sidebar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -16,44 +19,56 @@ const Sidebar = () => {
     <div>
       {/* Desktop Sidebar */}
       <div
-        className={`fixed top-0 bottom-0 left-0 h-screen  bg-primary p-2 text-secondary border border-y-transparent border-l-transparent border-r-gray-200 z-10 hidden
+        className={`fixed top-0 bottom-0 left-0 h-screen bg-primary p-2 text-secondary border border-y-transparent border-l-transparent border-r-gray-200 z-10 hidden
         md:relative  md:block ${isOpen ? "w-[200px]" : "w-full"}`}
       >
-        <button
-          onClick={toggleSidebar}
-          className="p-2 focus:outline-none hover:bg-indigo-200 rounded-[6px]"
-        >
-          <FiMenu size={24} />
-        </button>
-        <ul className="mt-10 space-y-2 font-medium">
-          <li>
-            <Link
-              href="/chats"
-              className="flex items-center space-x-4 py-4 px-2 hover:bg-indigo-200 rounded-[6px] cursor-pointer"
+        <div className="h-full relative">
+          <button
+            onClick={toggleSidebar}
+            className="p-2 focus:outline-none hover:bg-indigo-100 rounded-[6px]"
+          >
+            <FiMenu size={24} />
+          </button>
+
+          <ul className="mt-24 space-y-2 font-medium">
+            <li>
+              <Link
+                href="/chats"
+                className="flex items-center space-x-4 py-4 px-2 hover:bg-indigo-100 rounded-[6px] cursor-pointer"
+              >
+                <RiMessage3Fill size={24} />
+                {isOpen && <span className="text-lg">Messages</span>}
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/profile/1"
+                className="flex items-center space-x-4 py-4 px-2 hover:bg-indigo-100 rounded-[6px] cursor-pointer"
+              >
+                <FaUserAlt size={24} />
+                {isOpen && <span className="text-lg">Profile</span>}
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/settings"
+                className="flex items-center space-x-4 py-4 px-2 hover:bg-indigo-100 rounded-[6px] cursor-pointer"
+              >
+                <FaCog size={24} />
+                {isOpen && <span className="text-lg">Settings</span>}
+              </Link>
+            </li>
+          </ul>
+          <ul className="absolute bottom-8 w-full">
+            <li
+              className="flex items-center space-x-4 py-4 px-2 hover:bg-indigo-100 rounded-[6px] font-semibold cursor-pointer"
+              onClick={() => signOut()}
             >
-              <RiMessage3Fill size={24} />
-              {isOpen && <span className="text-lg">Messages</span>}
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="/profile"
-              className="flex items-center space-x-4 py-4 px-2 hover:bg-indigo-200 rounded-[6px] cursor-pointer"
-            >
-              <FaUserAlt size={24} />
-              {isOpen && <span className="text-lg">Profile</span>}
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="/settings"
-              className="flex items-center space-x-4 py-4 px-2 hover:bg-indigo-200 rounded-[6px] cursor-pointer"
-            >
-              <FaCog size={24} />
-              {isOpen && <span className="text-lg">Settings</span>}
-            </Link>
-          </li>
-        </ul>
+              <MdLogout size={24} />
+              {isOpen && <span className="text-lg">Log out</span>}
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
