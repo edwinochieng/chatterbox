afac17b0e9fc62331129f71813715e5fcfd49670
edwinochieng <ochiengedwin440@gmail.com>
2024-07-20 00:12:02 +0300

    add sidebar links

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,8 @@
 "use client";
+import Link from "next/link";
 import React, { useState } from "react";
-import { FaHome, FaUserAlt, FaCog } from "react-icons/fa";
+import { FaUserAlt, FaCog } from "react-icons/fa";
+import { RiMessage3Fill } from "react-icons/ri";
 import { FiMenu } from "react-icons/fi";
 
 const Sidebar = () => {
@@ -19,22 +21,37 @@ const Sidebar = () => {
       >
         <button
           onClick={toggleSidebar}
-          className="p-2 focus:outline-none hover:bg-indigo-400 rounded-[6px]"
+          className="p-2 focus:outline-none hover:bg-indigo-200 rounded-[6px]"
         >
           <FiMenu size={24} />
         </button>
-        <ul className="mt-10 space-y-4 font-semibold">
-          <li className="flex items-center space-x-4 p-2 hover:bg-indigo-200 rounded-[6px] cursor-pointer">
-            <FaHome size={24} />
-            {isOpen && <span className="text-lg">Home</span>}
+        <ul className="mt-10 space-y-2 font-medium">
+          <li>
+            <Link
+              href="/chats"
+              className="flex items-center space-x-4 py-4 px-2 hover:bg-indigo-200 rounded-[6px] cursor-pointer"
+            >
+              <RiMessage3Fill size={24} />
+              {isOpen && <span className="text-lg">Messages</span>}
+            </Link>
           </li>
-          <li className="flex items-center space-x-4 p-2 hover:bg-indigo-200 rounded-[6px] cursor-pointer">
-            <FaUserAlt size={24} />
-            {isOpen && <span className="text-lg">Profile</span>}
+          <li>
+            <Link
+              href="/profile"
+              className="flex items-center space-x-4 py-4 px-2 hover:bg-indigo-200 rounded-[6px] cursor-pointer"
+            >
+              <FaUserAlt size={24} />
+              {isOpen && <span className="text-lg">Profile</span>}
+            </Link>
           </li>
-          <li className="flex items-center space-x-4 p-2 hover:bg-indigo-200 rounded-[6px] cursor-pointer">
-            <FaCog size={24} />
-            {isOpen && <span className="text-lg">Settings</span>}
+          <li>
+            <Link
+              href="/settings"
+              className="flex items-center space-x-4 py-4 px-2 hover:bg-indigo-200 rounded-[6px] cursor-pointer"
+            >
+              <FaCog size={24} />
+              {isOpen && <span className="text-lg">Settings</span>}
+            </Link>
           </li>
         </ul>
       </div>
