b0a88afd8fedd26bf4799ffa59e7c5858f7aa674
edwinochieng <ochiengedwin440@gmail.com>
2024-07-31 19:07:28 +0300

    update textarea

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -1,15 +1,22 @@
+"use client";
 import React, { useState } from "react";
+import TextareaAutosize from "react-textarea-autosize";
+import { BsFillSendFill } from "react-icons/bs";
 
 export default function TextInput() {
   const [message, setMessage] = useState("");
   return (
-    <div>
-      <textarea
+    <div className="flex items-center border rounded-2xl py-2 px-4">
+      <TextareaAutosize
         value={message}
         placeholder="Type a message..."
-        className="w-full p-2 border rounded"
+        className="w-full p-4 text-xl outline-none resize-none overflow-y-auto hidden-scrollbar mr-3"
         onChange={(e) => setMessage(e.target.value)}
+        maxRows={5}
       />
+      <div className="cursor-pointer">
+        <BsFillSendFill size={28} className="text-accent" />
+      </div>
     </div>
   );
 }
