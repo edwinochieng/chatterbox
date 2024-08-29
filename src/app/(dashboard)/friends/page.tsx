b86e68cd59c689667eb723b38e5113c083c4c1ec
edwinochieng <ochiengedwin440@gmail.com>
2024-08-29 23:33:57 +0300

    add friends list

--- src/app/(dashboard)/friends/page.tsx
+++ src/app/(dashboard)/friends/page.tsx
@@ -1,5 +1,20 @@
+import FriendsList from "@/components/FriendsList";
+import MainNavbar from "@/components/MainNavbar";
 import React from "react";
 
 export default function MyFriendsPage() {
-  return <div>MyFriendsPage</div>;
+  return (
+    <div>
+      <div className="flex flex-row ">
+        <div className="w-full lg:max-w-[300px] xl:max-w-[380px]">
+          <FriendsList />
+        </div>
+
+        {/**Prompt */}
+        <div className="hidden lg:block w-full bg-gray-200 h-screen">
+          <h1 className=" text-gray-800">Hello world</h1>
+        </div>
+      </div>
+    </div>
+  );
 }
---
+++ src/components/FriendsList.tsx
@@ -0,0 +1,67 @@
+"use client";
+import React, { useState } from "react";
+import { CiSearch } from "react-icons/ci";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
+export default function FriendsList() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const friends = [
+    "Abel Abel",
+    "Cherry Mansfield",
+    "Drake Aubrey",
+    "Bianca Joe",
+    "Justin Timber",
+    "Halsey Winstead",
+    "Sandy Sims",
+    "Peter Parker",
+    "Lee Grant",
+  ];
+
+  const sortedFriends = friends.sort((a, b) => a.localeCompare(b));
+  const filteredFriends = sortedFriends.filter((friend) =>
+    friend.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+  return (
+    <div className="max-h-screen bg-primary">
+      <div className="h-full overflow-y-auto custom-scrollbar w-full ">
+        <div className="mt-5 px-6">
+          <h1 className="text-3xl font-semibold text-gray-800">My Friends</h1>
+
+          <div className="flex flex-row items-center justify-between p-1 px-4 w-full my-4 border rounded-[8px] bg-gray-100">
+            <div>
+              <CiSearch size={20} />
+            </div>
+            <input
+              type="text"
+              placeholder="Search chats"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+              className="flex-1 ml-2 py-2 border-none outline-none bg-transparent"
+            />
+          </div>
+        </div>
+
+        <div className="mt-4">
+          {filteredFriends.map((friend, index) => (
+            <div
+              key={index}
+              className="flex flex-row items-center space-x-4 my-1 py-2 px-6 cursor-pointer hover:bg-indigo-100"
+            >
+              <div>
+                <Avatar className="h-[42px] w-[42px]">
+                  <AvatarImage src="/profile.jpg" />
+                  <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+              </div>
+              <div>
+                <span className="text-gray-800 font-semibold text-xl">
+                  {friend}
+                </span>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
