4dfa2dc003b49cef19b6034671b8a3eff6cd09ae
edwinochieng <ochiengedwin440@gmail.com>
2024-08-30 18:32:45 +0300

    add friend requests section

--- src/components/AddFriends.tsx
+++ src/components/AddFriends.tsx
@@ -2,6 +2,7 @@
 import React, { useState } from "react";
 import { CiSearch } from "react-icons/ci";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { IoMdClose } from "react-icons/io";
 
 export default function AddFriends() {
   const [searchTerm, setSearchTerm] = useState("");
@@ -14,9 +15,21 @@ export default function AddFriends() {
     }
   };
 
+  const friendRequests = [
+    "Abel Abel",
+    "Cherry Mansfield",
+    "Drake Aubrey",
+    "Bianca Joe",
+    "Justin Timber",
+    "Halsey Winstead",
+    "Sandy Sims",
+    "Peter Parker",
+    "Lee Grant",
+  ];
+
   return (
     <div className="max-h-screen bg-primary">
-      <div className="h-full overflow-y-auto custom-scrollbar w-full ">
+      <div className="h-full w-full ">
         <div className="mt-5 px-6">
           <h1 className="text-3xl font-semibold text-gray-800">Add Friends</h1>
 
@@ -35,6 +48,7 @@ export default function AddFriends() {
         </div>
 
         <div className="mt-4">
+          {/**Search Results */}
           {result && (
             <div className="flex flex-row items-center space-x-4 my-1 py-2 px-6 cursor-pointer hover:bg-indigo-100">
               <div>
@@ -51,6 +65,47 @@ export default function AddFriends() {
                   johndoe@example.com
                 </span>
               </div>
+            </div>
+          )}
+
+          {/** Friend Requests */}
+          {!result && friendRequests && (
+            <div className="h-full overflow-y-auto hidden-scrollbar">
+              <div className="mt-10 mb-3 px-6">
+                <h1 className="text-gray-900 font-semibold text-2xl">
+                  Friend Requests
+                </h1>
+              </div>
+              {friendRequests.map((request, index) => (
+                <div key={index} className="flex justify-between px-6">
+                  <div className="flex flex-row items-center space-x-4 my-1 py-2 cursor-pointer ">
+                    <div>
+                      <Avatar className="h-[48px] w-[48px]">
+                        <AvatarImage src="/profile.jpg" />
+                        <AvatarFallback>CN</AvatarFallback>
+                      </Avatar>
+                    </div>
+                    <div className="flex flex-col">
+                      <span className="text-gray-800 font-semibold text-lg">
+                        {request}
+                      </span>
+                      <span className="text-gray-700 font-medium text-base">
+                        johndoe@example.com
+                      </span>
+                    </div>
+                  </div>
+                  <div className="flex items-center space-x-2">
+                    <div>
+                      <button className="text-white text-sm font-medium px-4 py-1.5 rounded-2xl bg-black  hover:bg-black/70">
+                        Accept
+                      </button>
+                    </div>
+                    <div className="cursor-pointer">
+                      <IoMdClose size={24} />
+                    </div>
+                  </div>
+                </div>
+              ))}
             </div>
           )}
         </div>
