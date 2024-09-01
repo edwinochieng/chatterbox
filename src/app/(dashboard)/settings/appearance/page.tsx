f5db5583968d9e53f21cd60e654550cba443d83f
edwinochieng <ochiengedwin440@gmail.com>
2024-09-01 23:35:48 +0300

    configure light/dark mode

--- src/app/(dashboard)/settings/appearance/page.tsx
+++ src/app/(dashboard)/settings/appearance/page.tsx
@@ -1,55 +1,28 @@
 "use client";
 
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import { z } from "zod";
-import { cn } from "@/lib/utils";
-import { Button, buttonVariants } from "@/components/ui/button";
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
-import { toast } from "@/components/ui/use-toast";
 import MainNavbar from "@/components/MainNavbar";
+import { useEffect, useState } from "react";
 
-const appearanceFormSchema = z.object({
-  theme: z.enum(["light", "dark"], {
-    required_error: "Please select a theme.",
-  }),
-  font: z.enum(["inter", "manrope", "system"], {
-    invalid_type_error: "Select a font",
-    required_error: "Please select a font.",
-  }),
-});
-
-type AppearanceFormValues = z.infer<typeof appearanceFormSchema>;
-
-// This can come from your database or API.
-const defaultValues: Partial<AppearanceFormValues> = {
-  theme: "light",
-};
 export default function AppearanceSettings() {
-  const form = useForm<AppearanceFormValues>({
-    resolver: zodResolver(appearanceFormSchema),
-    defaultValues,
-  });
+  const [activeTheme, setActiveTheme] = useState("light");
 
-  function onSubmit(data: AppearanceFormValues) {
-    toast({
-      title: "You submitted the following values:",
-      description: (
-        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
-          <code className="text-white">{JSON.stringify(data, null, 2)}</code>
-        </pre>
-      ),
-    });
-  }
+  useEffect(() => {
+    const theme = localStorage.getItem("theme");
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+    }
+  }, []);
+
+  const toggleLightMode = () => {
+    document.documentElement.classList.remove("dark");
+    localStorage.setItem("theme", "light");
+  };
+  const toggleDarkMode = () => {
+    document.documentElement.classList.add("dark");
+    localStorage.setItem("theme", "dark");
+    setActiveTheme("dark");
+  };
+
   return (
     <div>
       <MainNavbar
@@ -57,88 +30,67 @@ export default function AppearanceSettings() {
         description="Customize the appearance of the app. Automatically switch between day and night themes."
       />
       <div className="pt-12">
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-            <FormField
-              control={form.control}
-              name="theme"
-              render={({ field }) => (
-                <FormItem className="space-y-1">
-                  <FormLabel>Theme</FormLabel>
-                  <FormDescription>
-                    Select the theme for the dashboard.
-                  </FormDescription>
-                  <FormMessage />
-                  <RadioGroup
-                    onValueChange={field.onChange}
-                    defaultValue={field.value}
-                    className="grid max-w-md grid-cols-2 gap-8 pt-2"
-                  >
-                    <FormItem>
-                      <FormLabel className="[&:has([data-state=checked])>div]:border-gray-800">
-                        <FormControl>
-                          <RadioGroupItem value="light" className="sr-only" />
-                        </FormControl>
-                        <div className="items-center rounded-md border-2 border-muted p-1 hover:border-accent">
-                          <div className="space-y-2 rounded-sm bg-[#ecedef] p-2">
-                            <div className="space-y-2 rounded-md bg-white p-2 shadow-sm">
-                              <div className="h-2 w-[80px] rounded-lg bg-[#ecedef]" />
-                              <div className="h-2 w-[100px] rounded-lg bg-[#ecedef]" />
-                            </div>
-                            <div className="flex items-center space-x-2 rounded-md bg-white p-2 shadow-sm">
-                              <div className="h-4 w-4 rounded-full bg-[#ecedef]" />
-                              <div className="h-2 w-[100px] rounded-lg bg-[#ecedef]" />
-                            </div>
-                            <div className="flex items-center space-x-2 rounded-md bg-white p-2 shadow-sm">
-                              <div className="h-4 w-4 rounded-full bg-[#ecedef]" />
-                              <div className="h-2 w-[100px] rounded-lg bg-[#ecedef]" />
-                            </div>
-                          </div>
-                        </div>
-                        <span className="block w-full p-2 text-center font-normal">
-                          Light
-                        </span>
-                      </FormLabel>
-                    </FormItem>
-                    <FormItem>
-                      <FormLabel className="[&:has([data-state=checked])>div]:border-green-500">
-                        <FormControl>
-                          <RadioGroupItem value="dark" className="sr-only" />
-                        </FormControl>
-                        <div className="items-center rounded-md border-2 border-muted bg-popover p-1 hover:bg-accent hover:text-accent-foreground">
-                          <div className="space-y-2 rounded-sm bg-slate-950 p-2">
-                            <div className="space-y-2 rounded-md bg-slate-800 p-2 shadow-sm">
-                              <div className="h-2 w-[80px] rounded-lg bg-slate-400" />
-                              <div className="h-2 w-[100px] rounded-lg bg-slate-400" />
-                            </div>
-                            <div className="flex items-center space-x-2 rounded-md bg-slate-800 p-2 shadow-sm">
-                              <div className="h-4 w-4 rounded-full bg-slate-400" />
-                              <div className="h-2 w-[100px] rounded-lg bg-slate-400" />
-                            </div>
-                            <div className="flex items-center space-x-2 rounded-md bg-slate-800 p-2 shadow-sm">
-                              <div className="h-4 w-4 rounded-full bg-slate-400" />
-                              <div className="h-2 w-[100px] rounded-lg bg-slate-400" />
-                            </div>
-                          </div>
-                        </div>
-                        <span className="block w-full p-2 text-center font-normal">
-                          Dark
-                        </span>
-                      </FormLabel>
-                    </FormItem>
-                  </RadioGroup>
-                </FormItem>
-              )}
-            />
+        <div>
+          <h1 className="font-semibold text-xl">Theme</h1>
+          <p>Select the theme for the dashboard.</p>
+        </div>
 
-            <Button
-              type="submit"
-              className="text-white bg-black rounded-lg hover:bg-black/70"
+        <div className="mt-8 flex flex-row space-x-3">
+          {/**Light mode */}
+          <div>
+            <div
+              onClick={toggleLightMode}
+              className={`items-center cursor-pointer rounded-lg border-2  p-1  ${
+                activeTheme === "light" ? "border-gray-950" : "border-gray-50"
+              }`}
             >
-              Update Theme
-            </Button>
-          </form>
-        </Form>
+              <div className="space-y-2 rounded-sm bg-[#ecedef] p-2">
+                <div className="space-y-2 rounded-lg bg-white p-2 shadow-sm">
+                  <div className="h-2 w-[80px] rounded-lg bg-[#ecedef]" />
+                  <div className="h-2 w-[100px] rounded-lg bg-[#ecedef]" />
+                </div>
+                <div className="flex items-center space-x-2 rounded-lg bg-white p-2 shadow-sm">
+                  <div className="h-4 w-4 rounded-full bg-[#ecedef]" />
+                  <div className="h-2 w-[100px] rounded-lg bg-[#ecedef]" />
+                </div>
+                <div className="flex items-center space-x-2 rounded-lg bg-white p-2 shadow-sm">
+                  <div className="h-4 w-4 rounded-full bg-[#ecedef]" />
+                  <div className="h-2 w-[100px] rounded-lg bg-[#ecedef]" />
+                </div>
+              </div>
+            </div>
+            <span className="block w-full p-2 text-center font-normal">
+              Light
+            </span>
+          </div>
+          {/**Dark mode */}
+          <div>
+            <div
+              onClick={toggleDarkMode}
+              className={`items-center cursor-pointer rounded-lg border-2  p-1  ${
+                activeTheme === "dark" ? "border-gray-950 " : "border-gray-50"
+              }`}
+            >
+              <div className="space-y-2 rounded-sm bg-slate-950 p-2">
+                <div className="space-y-2 rounded-lg bg-slate-800 p-2 shadow-sm">
+                  <div className="h-2 w-[80px] rounded-lg bg-slate-400" />
+                  <div className="h-2 w-[100px] rounded-lg bg-slate-400" />
+                </div>
+                <div className="flex items-center space-x-2 rounded-lg bg-slate-800 p-2 shadow-sm">
+                  <div className="h-4 w-4 rounded-full bg-slate-400" />
+                  <div className="h-2 w-[100px] rounded-lg bg-slate-400" />
+                </div>
+                <div className="flex items-center space-x-2 rounded-lg bg-slate-800 p-2 shadow-sm">
+                  <div className="h-4 w-4 rounded-full bg-slate-400" />
+                  <div className="h-2 w-[100px] rounded-lg bg-slate-400" />
+                </div>
+              </div>
+            </div>
+            <span className="block w-full p-2 text-center font-normal">
+              Dark
+            </span>
+          </div>
+        </div>
       </div>
     </div>
   );
