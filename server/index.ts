e645d68a8646cb48513d5d854731c62c7a53caaa
edwinochieng <ochiengedwin440@gmail.com>
2024-09-03 04:13:41 +0300

    setup server

---
+++ server/index.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response } from "express";
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// Simple route to test the server
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello, TypeScript with Express and Nodemon!");
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
