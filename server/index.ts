ab1c7bc4c87fd5c2d3220a7b41eb64a14639cc80
edwinochieng <ochiengedwin440@gmail.com>
2024-09-07 00:21:19 +0300

    feat:change server port to 5000 to avoid conflict with frontend on 3000

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response } from "express";
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware to parse JSON bodies
 app.use(express.json());
